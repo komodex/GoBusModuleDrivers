a8d41626d96c093764d1942b93fcdae5c7416f1b
Matt Isenhower <matt@isenhower.com>
2012-05-22 12:12:43 -0700

    Adding initial solution structure.

---
+++ Komodex.NETMF.ModuleTestApp/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoGo;
+
+namespace Komodex.NETMF.ModuleTestApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+
+    }
+}
